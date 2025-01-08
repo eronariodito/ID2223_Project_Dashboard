4ef62eb585ad7521bff40879087d4a508b21b59a
eronariodito <67965868+eronariodito@users.noreply.github.com>
2025-01-08 21:06:22 +0100

    fix indexjs server

--- server/api/index.js
+++ server/api/index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const inferenceRouter = require("./routes/inference");
-const energyRouter = require("./routes/energy");
+const inferenceRouter = require("../routes/inference");
+const energyRouter = require("../routes/energy");
 
 const uri = process.env.DATABASE_URI;
 
