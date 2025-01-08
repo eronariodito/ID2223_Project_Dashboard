c334273affee1d5e7fd9c06ea6df370c26726308
eronariodito <67965868+eronariodito@users.noreply.github.com>
2025-01-08 21:41:45 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+var cors = require("cors");
+
+app.use(cors());
+
 const mongoose = require("mongoose");
 
 const inferenceRouter = require("./routes/inference");
